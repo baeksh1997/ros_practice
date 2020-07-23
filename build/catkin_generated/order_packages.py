cd7936e0a7a44b196b0d2ea6866ad25499df5187
Baek <baeksh1997@naver.com>
2020-07-23 17:52:30 +0900

    ros practice

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/baeksh1997/catkin_ws2/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/baeksh1997/catkin_ws2/devel;/opt/ros/melodic'.split(';') if '/home/baeksh1997/catkin_ws2/devel;/opt/ros/melodic' != '' else []
